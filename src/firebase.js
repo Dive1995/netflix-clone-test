b7e4f9042a1d7a27a044a3c0f60dca59b4c7d281
Dive <Diveshan1995@gmail.com>
2021-04-21 11:52:30 +0530

    finished login and register

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
     apiKey: "AIzaSyCb2l_cm249C4SlJPOLX_35i49bgbnRSUk",
     authDomain: "netflix-clone-9059e.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
     messagingSenderId: "549371845779",
     appId: "1:549371845779:web:3d9ea7394456f7032d4166"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebaseApp.auth();
+
+  export {auth};
+  export default db;
--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,23 +1,54 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { auth } from '../firebase'
 import './SignUpScreen.css'
 
 function SignUpScreen() {
+
+    const emailRef = useRef() //////// consider useRef as a finger wich -
+    const passwordRef = useRef() /////  -points the html element on the page
+
+  
+    const handleRegister = (e) => {
+        e.preventDefault()
+        auth
+            .createUserWithEmailAndPassword(    //takes two arguments (email,password)
+                emailRef.current.value,
+                passwordRef.current.value
+            )
+            .then((authUser) => {   ///if success
+                console.log(authUser);
+            })
+            .catch((error) => {
+                alert(error.message)
+            })
+    }
+
     const handleSignIn = (e) =>{
         e.preventDefault()
+        auth
+            .signInWithEmailAndPassword(
+                emailRef.current.value,
+                passwordRef.current.value
+            ).then((authUser) => {
+                console.log(authUser);
+            })
+            .catch((error) => {
+                alert(error.message)
+            })
     }
 
     return (
         <div className="signupScreen">
             <form>
                 <h1>Sign In</h1>
-                <input type="email" placeholder="Email"/>
-                <input type="password" placeholder="Password"/>
+                <input ref={emailRef} type="email" placeholder="Email"/>
+                <input ref={passwordRef} type="password" placeholder="Password"/>
                 <button onClick={handleSignIn}>Sign In</button>
                 <h4>
                     <span className="signupScreen__gray">
                         New to Netflix? 
                     </span>
-                    <span className="signupScreen__link">
+                    <span onClick={handleRegister} className="signupScreen__link">
                         Sign Up now.
                     </span>
                 </h4>
